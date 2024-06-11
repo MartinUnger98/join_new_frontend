2700fb5a750b171482d35dbc80f9d62da5a28468
Martin Unger <martin-unger2@live.at>
2024-06-11 14:43:46 +0200

    added tailwind styled login

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Passe den Pfad an deine Projektstruktur an
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '110': '30rem',
+      },
+      colors: {
+        'blue-primary': '#29ABE2',
+        'blue-dark': '#2a3647',
+        'gray-input': '#cbd5e1'
+      },
+      boxShadow: {
+        'custom': '0px 0px 17px 5px rgba(0, 0, 0, 0.08)',
+      },
+    },
+  },
+  plugins: [],
+}
