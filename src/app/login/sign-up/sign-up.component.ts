4279682cf4438d1351808decf55e4da88f7f4e83
Martin Unger <martin-unger2@live.at>
2024-06-13 22:22:03 +0200

    changed toast and clean coded onSubmit function

--- src/app/login/sign-up/sign-up.component.ts
+++ src/app/login/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, forwardRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
 import Validation from './password-match.validator';
 import { AuthService } from 'src/app/services/auth.service';
@@ -23,7 +23,6 @@ export class SignUpComponent implements OnInit {
     confirmPassword: new FormControl(''),
     acceptTerms: new FormControl(false),
   })
-  submitted = false;
 
   constructor(private formBuilder: FormBuilder, public authService: AuthService, private messageService: MessageService, private router: Router) {}
 
@@ -31,23 +30,9 @@ export class SignUpComponent implements OnInit {
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group(
       {
-        username: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(20)
-          ]
-        ],
+        username: ['',[Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
         email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(8),
-            Validators.maxLength(40)
-          ]
-        ],
+        password: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(40)]],
         confirmPassword: ['', Validators.required],
         acceptTerms: [false, Validators.requiredTrue]
       },
@@ -62,35 +47,45 @@ export class SignUpComponent implements OnInit {
   }
 
   async onSubmit() {
-    this.submitted = true;
     let signUpJson = this.signUpForm.value;
     try {
-      this.isLoading = true;
-      await this.authService.registerUser(signUpJson.username, signUpJson.password, signUpJson.email);
-      this.isLoading = false;
-      this.showSuccess();
-      setTimeout(() => {
-        this.router.navigate(["/login"])
-        this.resetForm();
-      }, 3000);
+      this.registerUser(signUpJson);
     } catch (error:any) {
-      this.isLoading = false;
+      this.setToastErrorMessage(error);
+    }
+  }
+
+  async registerUser(signUpJson: any) {
+    this.isLoading = true;
+    await this.authService.registerUser(signUpJson.username, signUpJson.password, signUpJson.email);
+    this.isLoading = false;
+    this.showSuccess();
+    setTimeout(() => {
+      this.router.navigate(["/login"])
+      this.resetForm();
+    }, 3000);
+  }
+
+  setToastErrorMessage(error: any) {
+    this.isLoading = false;
+    if (error.status === 400 && error.error.username) {
+      this.messageService.add({ severity: 'error', summary: 'Registration Failed', detail: error.error.username[0] });
+    } else if(error.status === 400 && error.error.email) {
+      this.messageService.add({ severity: 'error', summary: 'Registration Failed', detail: error.error.email[0] });
+    } else {
       this.showError();
-      console.error('Error during registration', error);
-      console.error('Error details:', error.error);
     }
   }
 
   showSuccess() {
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'You' });
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Your account has been created successfully!' });
   }
 
   showError() {
-    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Message Content' });
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'An unexpected error occurred!' });
   }
 
   resetForm(): void {
-    this.submitted = false;
     this.signUpForm.reset();
   }
 
@@ -102,3 +97,4 @@ export class SignUpComponent implements OnInit {
     }
   }
 }
+
