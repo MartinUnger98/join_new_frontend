fe84957d97fbace790aff8b7dfdf727ce922b9f3
Martin Unger <martin-unger2@live.at>
2024-06-15 23:38:01 +0200

    sorted contacts alphabetically and grouped them

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,23 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Contact } from './contact.model';
 
 @Component({
   selector: 'app-contacts',
-  standalone: false,
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
-  contacts: any = [];
-  constructor(private http:HttpClient) { }
+  contacts: Contact[] = [];
+  contactsInitials: string[] = [];
+
+
+  constructor(private http: HttpClient) {}
 
   async ngOnInit() {
-    this.contacts = await this.loadContacts();
+    let contacts = await this.loadContacts();
+    this.contacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
+    this.getContactsInitials();
+
   }
 
-  loadContacts() {
+  loadContacts(): Promise<Contact[]> {
     const url = environment.baseUrl + '/contacts/';
-    return lastValueFrom(this.http.get(url));
+    return lastValueFrom(this.http.get<Contact[]>(url));
   }
+
+  getContactsInitials() {
+    this.contacts.forEach(contact => {
+      this.contactsInitials.push(contact.name.charAt(0))
+    })
+  }
+
 }
