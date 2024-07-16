0701c74d497c00c1c222530de352f93494c30c3e
Martin Unger <martin-unger2@live.at>
2024-07-16 18:12:46 +0200

    added ng-template to p-multiselect and connected with dialog-contact

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { Contact } from '../contacts/contact.model';
 import { BackendServicesService } from '../services/backend-services.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
+  providers: [MessageService],
 })
-export class AddTaskComponent implements OnInit{
+export class AddTaskComponent implements OnInit, AfterViewInit, OnDestroy{
   addTaskForm: FormGroup = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
@@ -21,17 +23,23 @@ export class AddTaskComponent implements OnInit{
 
   contacts: Contact[] = [];
   contactsInitials: string[] = [];
+  showDialog: boolean = false;
   private destroyed$ = new Subject<void>();
 
 
   constructor(
     private cdRef: ChangeDetectorRef,
     private backendService: BackendServicesService,
+    private messageService: MessageService,
   ) {}
 
   async ngOnInit() {
     this.subscribeObservables();
     await this.backendService.loadContacts();
+  }
+
+  ngAfterViewInit() {
+    this.cdRef.detectChanges();
   }
 
   subscribeObservables() {
@@ -49,6 +57,19 @@ export class AddTaskComponent implements OnInit{
     let nameParts = contactName.split(' ');
     let initials = nameParts.slice(0, 2).map((part) => part[0].toUpperCase());
     return initials.join('');
+  }
+
+  toggleDialog() {
+    this.showDialog = true;
+  }
+
+  closeDialog(success: boolean) {
+    this.showDialog = false;
+    if (success) {
+      this.messageService.add({ severity:'success', summary: 'Success', detail: 'You have successfully added a contact!' });
+    } else {
+      this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+    }
   }
 
   ngOnDestroy() {
